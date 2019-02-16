2566a7b81d8921b66b043270c97921f8c613ed1e
Erin <erinwu25@gmail.com>
2019-02-15 22:50:52 -0800

    first four lines

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ Created on Feb 15, 2019
 @author: dfa
 '''
 #yay first push
+from pprint import pprint
+import requests
+r = requests.get('http://api.openweathermap.org/data/2.5/weather?q=London&APPID=386aa376a85049b9a45c5fb223f1a691')
+pprint(r.json())
